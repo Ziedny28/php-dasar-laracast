878b3f5ea26ffa39bfc99f12fd3aa9b5326ae5ad
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-03-05 08:15:10 +0700

    superglobals and current page styling

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+require 'functions.php';
+
 //this is a controller
 $heading = "Home";
-require "views/index.view.php";
+require "views/index.view.php"; 
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,10 +8,11 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="index.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="about.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
-              <a href="contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</a>
-              
+              <!-- the directory for comparison depends on your file locations -->
+              <a href="index.php" class="<?= urlIs("/dasarWebLanjutan/demo/index.php") ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="about.php" class="<?=urlIs('/dasarWebLanjutan/demo/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
+              <a href="contact.php" class="<?=urlIs('/dasarWebLanjutan/demo/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</a>
+
             </div>
           </div>
         </div>
