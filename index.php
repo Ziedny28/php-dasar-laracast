aa69e8821d06d5f8027fccef3b5a97a5333d9895
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-03-06 12:13:14 +0700

    cleaning unnecesery stuff

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ require 'functions.php';
 
 function ParseUrl(){
     if(isset($_GET['url'])){
-        $url = rtrim($_GET['url'],'/');
-        $url = filter_var($url,FILTER_SANITIZE_URL);
+        $uri = rtrim($_GET['url'],'/');
+        $uri = filter_var($uri,FILTER_SANITIZE_URL);
         
-        return $url;
+        return $uri;
     }
 }
-
+var_dump(ParseUrl());
 //so now i can do a routing or smth, in XAMP, an outdated s-
