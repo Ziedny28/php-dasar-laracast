ba95e00aab5ab992ddf2dee523697112f4d20459
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-03-07 09:28:40 +0700

    make a php router refactored

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
+//run it with 'php -S localhost:8888'..
 
 require 'functions.php';
 
-//or i can just run it with php -S localhost:8888 so..
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if($uri === '/'){
-    require('controllers/index.php');
-}
-if($uri === '/about'){
-    require('controllers/about.php');
-}
-if($uri === '/contact'){
-    require('controllers/contact.php');
-}
+require 'router.php';
