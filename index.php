f0e01763d6e2dbbd9dec012c0ab4e368523b62e7
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-03-07 10:28:04 +0700

    blueprint in php demonstrated

--- index.php
+++ index.php
@@ -3,4 +3,21 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+//connet to mysql database
+
+class Person{
+    public $name;
+    public $age;
+
+    public function Breathe(){
+        echo $this->name.' is breathin'; 
+    }
+}
+
+$person = new Person();
+$person->name='John Doe';
+$person->age=25;
+
+$person->Breathe();
